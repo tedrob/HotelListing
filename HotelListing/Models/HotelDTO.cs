8b88e5ef43be9c918bac647b2d411ba424c0ba32
tedrobinson <tfqbtd@aol.com>
2022-01-13 03:00:47 -0500

    Added Database Automapper, Unit of work and DTOs

---
+++ HotelListing/Models/HotelDTO.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class CreateHotelDTO
+    {       
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel Name Is To Long")]
+
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 250, ErrorMessage = "Address Name Is To Long")]        
+        public string? Address { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public double Rating { get; set; }
+        
+        public int CountryId { get; set; }        
+    }
+
+    public class HotelDTO : CreateHotelDTO
+    {
+        public int Id { get; set; }
+        public CountryDTO? Country { get; set; }
+        
+        
+    }
+}
--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -1,11 +1,16 @@
+using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
+using HotelListing.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(o =>
@@ -15,7 +20,9 @@ builder.Services.AddCors(o =>
         .AllowAnyMethod()
         .AllowAnyHeader());
 });
+builder.Services.AddAutoMapper(typeof(MapperInitializer));
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 builder.Host.UseSerilog ((ctx, lc) => lc
     .WriteTo.Console()
